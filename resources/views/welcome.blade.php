0ef1a3cffd34a7280e9bfa6f486da820f2c23910
sebbn <sebastian.westlinder@gmail.com>
2017-11-24 16:20:16 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    INIT COMMIT
+                    INIT COMMIT 2
                 </div>
 
                 <div class="links">
